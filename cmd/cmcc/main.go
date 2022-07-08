c94dee21beca545a64776f09e9d44aa57e13177c
黄仲辉 <610195979@qq.com>
2022-07-08 19:45:26 +0800

    增加String()方法。

--- cmd/cmcc/main.go
+++ cmd/cmcc/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"sms-vgateway/cmcc"
+	"sms-vgateway/snowflake"
+	"sms-vgateway/snowflake32"
 )
 
 func main() {
+	cmcc.Sequence32 = snowflake32.NewSnowflake(cmcc.Conf.DataCenterId, cmcc.Conf.WorkerId)
+	cmcc.Sequence64 = snowflake.NewSnowflake(int64(cmcc.Conf.DataCenterId), int64(cmcc.Conf.WorkerId))
 	cmcc.StartServer()
 }
--- cmd/cmcc/server_test.go
+++ cmd/cmcc/server_test.go
@@ -29,7 +29,7 @@ var (
 	termChan  = make(chan struct{})
 
 	clients  = 1
-	duration = time.Second * 10
+	duration = time.Second * 100
 	// addr = "10.211.55.13:9000"
 	addr = ":9000"
 )
